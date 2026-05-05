0e8d6c389e739f720240ffefdda6df1f6243c49f
Rutger Pronk <rutger.pronk11@gmail.com>
2026-05-05 21:02:06 +0200

    Add printboard

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
 use std::io;
 use std::io::Write;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum FieldValue {
     Empty,
     Cross,
     Circle,
+}
+impl std::fmt::Display for FieldValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FieldValue::Empty => write!(f, ""),
+            FieldValue::Cross => write!(f, "X"),
+            FieldValue::Circle => write!(f, "O"),
+        }
+    }
 }
 
 fn main() {
     let mut board = vec![
         vec![FieldValue::Empty, FieldValue::Empty, FieldValue::Empty],
         vec![FieldValue::Empty, FieldValue::Empty, FieldValue::Empty],
-        vec![FieldValue::Empty, FieldValue::Empty, FieldValue::Empty]
+        vec![FieldValue::Empty, FieldValue::Empty, FieldValue::Empty],
     ];
 
     board[0][1] = FieldValue::Cross;
 
-    println!("{:?}", board);
+    print_board(board);
 
     get_user_coordinate();
 
@@ -25,7 +34,6 @@ fn main() {
 }
 
 fn get_user_coordinate() {
-
     let number = get_user_number(1, 3);
 
     println!("User choose: {number}");
@@ -45,4 +53,19 @@ fn get_user_number(min: i32, max: i32) -> i32 {
     }
 
     input.parse().expect("While loop should validate")
+}
+
+fn print_board(board: Vec<Vec<FieldValue>>) {
+    for row in 0..3 {
+        for column in 0..3 {
+            print!(" {:^1}", board[row][column].to_string());
+            if column != 2 {
+                print!(" |");
+            }
+        }
+        println!();
+        if row != 2 {
+            println!("-----------");
+        }
+    }
 }
